e7274ff14fff454b61cc390a1adf4018ca203e55
EricKiku <2966678301@qq.com>
2024-02-29 16:15:12 +0800

    base blog

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -7,20 +7,4 @@ export default navbar([
     link: "/",
     activeMatch: "^/",
   },
-  // {
-  //   text: "主页",
-  //   link: "/",
-  // },
-
-  // {
-  //   text: "博文",
-  //   icon: "boke",
-  //   activeMatch: "^/posts",
-  //   link: "/posts/",
-  // },
-  // {
-  //   text: "V2 文档",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
 ]);
---
+++ src/article/base.md
@@ -0,0 +1,7 @@
+---
+title: BASE
+category:
+  - blog
+---
+
+## BASE
